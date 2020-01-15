6df1f81eada5594c7ff2afc7f0512b603ecce733
jamespnixon <58857800+jamespnixon@users.noreply.github.com>
2020-01-15 08:30:21 -0800

    WAAPI_GetInfo First Commit

---
+++ WAAPI_GetInfo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWAAPI
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            _Main().Wait();
+        }
+
+        static async Task _Main()
+        {
+            AK.Wwise.Waapi.JsonClient client = new AK.Wwise.Waapi.JsonClient();
+            await client.Connect();
+            client.Disconnected += () =>
+            {
+                System.Console.WriteLine("We lost connection!");
+            };
+            JObject info = await client.Call(ak.wwise.core.getInfo, null, null);
+            System.Console.WriteLine(info);
+            Console.WriteLine("end");
+        }
+    }
+}
+
